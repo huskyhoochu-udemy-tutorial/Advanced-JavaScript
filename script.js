217f41bb873249e86ba94ecc94a51f1e0dfd567b
huskyhoochu <dfg1499@gmail.com>
2018-01-15 20:45:15 +0900

    practice updated

--- script.js
+++ script.js
@@ -54,4 +54,73 @@ console.log(obj.city);
 */
 
 // Passing functions as arguments
+/*
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var heartRate = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(heartRate);
+*/
+
+// Functions returning functions
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ', hello designer');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log(name + ', hello teacher');
+        }
+    } else {
+        return function (name) {
+            console.log(name + ', who are you?');
+        }
+    }
+}
+
+var teacher = interviewQuestion('teacher');
+var john = teacher('john');
+
+var designer = interviewQuestion('designer');
+var jane = designer('jane');
+
+var none = interviewQuestion('none');
+var tom = none('tom');
+
+console.log(john);
+console.log(jane);
+console.log(tom);
+*/
+
+// Immediately Invoked Function Expressions (IIFE)
 
